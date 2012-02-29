8b4b882bb99a0e34519afcc07febc6c38ecb033c
Dennis Linuz <dennismald@gmail.com>
2012-02-29 10:17:09 -0600

    new file:   bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#############################################################
+# Bubblesort for Python                                     #
+# by: Dennis Linuz <dennismald@gmail.com>                   #
+# Demonstration of a bubble 		                        #
+#############################################################
+
+import sys,time
+
+input_array=[]
+if (len(sys.argv) < 2):
+    print ""
+    print "Please input list items as arguments"
+    print "\nExample: ./bogosort.py <item1> [item2] [item3] [item4] ..."
+    quit()
+i=1
+while (i < len(sys.argv)):
+    input_array.append(int(sys.argv[i]))
+    i+=1
+
+def bubbleSort(array):
+	length=len(array)
+	result = True
+	global count
+	while result:
+		result = False
+		i=0
+		while (i < length-1):
+			if (array[i] > array[i+1]):
+				tempVar = array[i]
+				array[i] = array[i+1]
+				array[i+1] = tempVar
+				result = True
+			i=i+1
+			count+=1
+			print "Sorting: " + str(array)
+	return array
+count = 0
+time1 = time.time()
+arrayResult = str(bubbleSort(input_array))
+print ""
+print "Sorted after " + str(count) + " tries."
+print "Sorted:  " + arrayResult 
+print "---"
+print "Overall Time: " + str(time.time()-time1) + " seconds"
