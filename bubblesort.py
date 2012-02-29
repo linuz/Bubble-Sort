dd439af503828c59f90b6da54dd7435053c60ead
Dennis Linuz <dennismald@gmail.com>
2012-02-29 10:25:11 -0600

    Documentation fix

--- bubblesort.py
+++ bubblesort.py
@@ -11,7 +11,7 @@ input_array=[]
 if (len(sys.argv) < 2):
     print ""
     print "Please input list items as arguments"
-    print "\nExample: ./bogosort.py <item1> [item2] [item3] [item4] ..."
+    print "\nExample: ./bubblesort.py <item1> [item2] [item3] [item4] ..."
     quit()
 i=1
 while (i < len(sys.argv)):
